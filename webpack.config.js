c27fda3a7f825a6b32964be3d5eb7543d5c068c0
nyo <billy31210@gmail.com>
2018-04-09 14:18:12 +0800

    add babel

--- webpack.config.js
+++ webpack.config.js
@@ -11,28 +11,15 @@ const config = {
     filename: '[name].bundle.js',
     publicPath: '/'
   },
-  //devtool: 'source-map',
-  //module: {
-    //rules: [
-      //{
-        //test: /\.css$/,
-        //use: ['style-loader', 'css-loader']
-      //},
-      //{
-        //test: /\.(js|jsx)$/,
-        //exclude: /node_modules/,
-        //loader: 'babel-loader'
-      //},
-      //{
-        //test: /\.(png|jpg|jpeg|gif|svg)$/,
-        //loader: 'file-loader'
-      //},
-      //{
-        //test: /\.(ttf|eot|svg|woff(2)?)$/,
-        //loader: 'file-loader'
-      //}
-    //]
-  //},
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({ template: './src/index.html' })
   ],
